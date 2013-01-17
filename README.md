99702902ae55d674f8c49ba069ba463a3abe96fa
Daniel King <daniel@fake.fake>
2013-01-17 08:53:13 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 brulizer
 ========
 
-A command line Ruby script to translate English into the words of Dr. Steve Brule
+A command line Ruby script to translate English into the words of Dr. Steve Brule.
+
+With Ruby installed just type `ruby brule.rb` followed by each word you want to translate separated by single spaces.
+For example, `ruby brule.rb David Liebe Hart`.
---
+++ brule.rb
@@ -0,0 +1,53 @@
+@dictionary = { 
+	'man' => 'hunk',
+	'guy' => 'hunk',
+	'men' => 'hunks',
+	'guys' => 'hunks',
+	'pringle-brule' => 'Pringle-Brule-Salahari', 
+	'johnny' => 'Jimmy',
+	'boden' => 'Brongus',
+	'david' => 'Daniel',
+	'liebe' => 'Drungle',
+	'hart' => 'Hart',
+	'don' => 'Dan',
+	'davis' => 'Dungus',
+	'rodriguez' => 'Rongriguez',
+	'jackpot' => 'jackprot',
+	'pizza' => 'preetza',
+	'bruce' => 'Bringus',
+	'observatory' => 'big old dirty church',
+
+	# Unchanged words
+	'hart' => 'Hart',
+	'ron' => 'Ron',
+	'don' => 'Don',
+	'volante' => 'Volante',
+}
+
+@words = []
+ARGV.each do |word|
+	@words << word.dup
+end
+
+@words.each do |word|
+	if @dictionary.include? word.downcase
+		word.replace @dictionary[word.downcase]
+	elsif word.length > 2
+		unless (word[1] == 'r') || (word[0] == 'r') || (rand(2) == 1)
+			word.insert(1, 'r')
+		end
+		if (word[-1] == 'l') && (word[-2] == 'e')
+			word[-1] = 'e'
+			word[-2] = 'l'
+			word[-3] = 'g'
+		end
+		if word[-1] == 's'
+			word[-2] = 'u'
+		end
+		if word.length > 11
+			word.replace 'dingus'
+		end
+	end
+end
+
+puts @words.join(' ')
